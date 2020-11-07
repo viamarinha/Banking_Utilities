48c54fb4d776b3354ab3a487ae66527a2a3d8cb7
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-06 22:11:09 -0500

    Create  a class for a  ProgressDialog.java for a functionality

---
+++ src/andrylat/bankingutilities/dev/dialog/ProgressDialog.java
@@ -0,0 +1,39 @@
+package andrylat.bankingutilities.dev.dialog;
+
+import andrylat.bankingutilities.dev.interfaces.ValidatorInt;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class ProgressDialog {
+
+    private ValidatorInt validatorInt;
+
+    public ValidatorInt getCustomerInteraction() {
+        return validatorInt;
+    }
+
+    public void setCustomerInteraction(ValidatorInt validatorInt) {
+        this.validatorInt = validatorInt;
+    }
+
+    public  String[] getCustomerData() {
+        System.out.println("Enter a card number with 16 digits");
+        System.out.println("Ex : xxxx-xxxx-xxxx-xxxx or \n xxxx xxxx xxxx xxxx");
+        Scanner scanner = new Scanner(System.in);
+        String customerCardNumber = scanner.nextLine();
+        System.out.println("Enter a control number with 3 digits");
+        String controlNumber = scanner.nextLine();
+        return new String[]{customerCardNumber, controlNumber};
+    }
+
+    public   <K, V> void showErrorsLog(Map<K, V> errorResult) {
+        for (Map.Entry<K, V> entry : errorResult.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    public Map<String, String> interactionWithCustomer(String[] customerInput) {
+        return validatorInt.interactionWithCustomer(customerInput);
+    }
+}
