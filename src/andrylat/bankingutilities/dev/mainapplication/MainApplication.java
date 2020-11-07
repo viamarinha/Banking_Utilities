a6a5ce6f9d859bb7c08d3a2f94626776f89dfc72
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-06 22:12:17 -0500

    Create  a MainApplication.java   class

---
+++ src/andrylat/bankingutilities/dev/mainapplication/MainApplication.java
@@ -0,0 +1,37 @@
+package andrylat.bankingutilities.dev.mainapplication;
+
+import andrylat.bankingutilities.dev.dialog.ProgressDialog;
+import andrylat.bankingutilities.dev.interfaces.ValidatorInt;
+import andrylat.bankingutilities.dev.validators.BankingCardValidator;
+
+import java.util.Map;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        reRun();
+    }
+
+    private static void reRun() {
+
+        ProgressDialog progressDialog = new ProgressDialog();
+        ValidatorInt bankingCardValidator = new BankingCardValidator();
+        progressDialog.setCustomerInteraction(bankingCardValidator);
+
+        String[] customerCardNumber = progressDialog.getCustomerData();
+
+        Map<String, String> errorResult = progressDialog.interactionWithCustomer(customerCardNumber);
+        if (errorResult.entrySet().stream().findFirst().get().getValue().equalsIgnoreCase("ok"))
+            paymentSystem();
+        else {
+            progressDialog.showErrorsLog(errorResult);
+            reRun();
+        }
+    }
+
+    private static void paymentSystem() {
+        System.out.println("You are in main apps");
+    }
+
+
+}
