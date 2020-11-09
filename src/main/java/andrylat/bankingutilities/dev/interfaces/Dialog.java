5a93484ad477b1b00fc8769745c4af36de8feb75
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-08 20:07:25 -0500

    Create an interface for ProgressDialog.java .

---
+++ src/main/java/andrylat/bankingutilities/dev/interfaces/Dialog.java
@@ -0,0 +1,14 @@
+package andrylat.bankingutilities.dev.interfaces;
+
+import java.util.Map;
+
+public interface Dialog {
+
+    String[] getCustomerData();
+
+    <K, V> void showErrorsLog(Map<K, V> errorResult);
+
+    Map<String, String> interactionWithCustomer(String[] customerInput);
+
+    void setCustomerInteraction(ValidatorInt bankingCardValidator);
+}
