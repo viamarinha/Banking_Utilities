7fbbc1327ac0137aedc3715bc3749c4ad731fb3a
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-11 09:50:38 -0500

    Change name some attributes adapt a logic for a new return type of List and not Map

--- src/main/java/dev/andrylat/bankingutilities/validators/CardValidatorImpl.java
+++ src/main/java/dev/andrylat/bankingutilities/validators/CardValidatorImpl.java
@@ -1,29 +1,28 @@
 package dev.andrylat.bankingutilities.validators;
 
-import dev.andrylat.bankingutilities.interfaces.ValidatorInt;
-
+import dev.andrylat.bankingutilities.interfaces.CardValidator;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BankingCardValidator implements ValidatorInt {
+public class CardValidatorImpl implements CardValidator {
 
-    private final Map<String, String> resultValidator = new HashMap<>();
-    private final String cardRegex = "([2-6]([0-9]{3})(\\s|-)?)(([0-9]{4}(\\s|-)?){3})";
-    private final String numericDigits = "[0-9]+";
-    private String cleanCustomerInput;
+    private static final String cardRegex = "([2-6]([0-9]{3})(\\s|-)?)(([0-9]{4}(\\s|-)?){3})";
+    private static final String numericDigits = "[0-9]+";
+    private static final String spaceDashRegex = "[\\s-]";
+    private static final int cardSize = 16;
+    private static final int maxDigitValue = 9;
+    private final List<String> resultValidator = new ArrayList<>();
 
-    public Map<String, String> interactionWithCustomer(String customerInput) {
+    public List<String> interactionWithCustomer(String customerInput) {
 
         Pattern cardNumberPattern = Pattern.compile(cardRegex);
         Matcher cardMatcher = cardNumberPattern.matcher(customerInput);
-        cleanCustomerInput = cleanCustomerInput(customerInput);
         if (cardMatcher.matches()) {
             if (validateControlNumber(customerInput)) {
-                resultValidator.put("ok", "ok");
+                resultValidator.add("ok");
                 return resultValidator;
             }
-
         } else {
             cardsErrorsTreatments(customerInput, cardMatcher);
         }
@@ -32,24 +31,25 @@ public class BankingCardValidator implements ValidatorInt {
 
     private void cardsErrorsTreatments(String customerInput, Matcher cardMatcher) {
 
+        String cleanCustomerInput = format(customerInput);
         if (!cardMatcher.matches()) {
             List<Integer> validFirstChar = Arrays.asList(2, 3, 4, 5, 6);
             int firstChar = Character.getNumericValue(customerInput.toCharArray()[0]);
             if (!validFirstChar.contains(firstChar))
-                resultValidator.put("Card number problem : ", "wrong card number \n card number should starts within 2 - 6");
+                resultValidator.add("wrong card number \n card number should starts within 2 - 6");
         }
-        if (cleanCustomerInput.length() < 16)
-            resultValidator.put("Card number problem : ", "not enough digits in card number");
-        if (cleanCustomerInput.length() > 16)
-            resultValidator.put("Card number problem : ", "too many digits in card number");
+        if (cleanCustomerInput.length() < cardSize)
+            resultValidator.add("not enough digits in card number");
+        if (cleanCustomerInput.length() > cardSize)
+            resultValidator.add("too many digits in card number");
         if (!cleanCustomerInput.matches(numericDigits))
-            resultValidator.put("Card number problem :", "you have entered not a numeric digits");
+            resultValidator.add("you have entered not a numeric digits");
     }
 
     private boolean validateControlNumber(String customerInput) {
 
+        String cleanCustomerInput = format(customerInput);
         List<Integer> creditCardInt = new LinkedList<>();
-
 
         for (int i = 0; i < cleanCustomerInput.length(); i++) {
             creditCardInt.add(Integer.parseInt(cleanCustomerInput.substring(i, i + 1)));
@@ -58,23 +58,23 @@ public class BankingCardValidator implements ValidatorInt {
         for (int count = creditCardInt.size() - 2; count >= 0; count -= 2) {
             int tmpValue = creditCardInt.get(count);
             tmpValue *= 2;
-            if (tmpValue > 9)
+            if (tmpValue > maxDigitValue)
                 tmpValue = tmpValue % 10 + 1;
             creditCardInt.set(count, tmpValue);
         }
 
-        int lstSum = creditCardInt.stream().mapToInt(Integer::intValue).sum();
+        int SumAllDigits = creditCardInt.stream().mapToInt(Integer::intValue).sum();
 
-        if (lstSum % 10 == 0) {
+        if (SumAllDigits % 10 == 0) {
             return true;
         } else {
-            resultValidator.put("Card number problem :", "incorrect control number");
+            resultValidator.add("incorrect control number");
             return false;
         }
     }
 
-    public String cleanCustomerInput(String customerInput) {
-        String cleanCustomerInput = customerInput.replaceAll("[\\s-]", "");
+    private String format(String customerInput) {
+        String cleanCustomerInput = customerInput.replaceAll(spaceDashRegex, "");
         return cleanCustomerInput;
     }
 
