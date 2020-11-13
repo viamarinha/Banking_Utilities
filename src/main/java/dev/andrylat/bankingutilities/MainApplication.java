972a386f8c989b551515e5d4792107edcc900c6a
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-13 09:37:19 -0500

    Ass methods for a System payments

--- src/main/java/dev/andrylat/bankingutilities/MainApplication.java
+++ src/main/java/dev/andrylat/bankingutilities/MainApplication.java
@@ -1,12 +1,16 @@
 package dev.andrylat.bankingutilities;
 
+import dev.andrylat.bankingutilities.Enumerations.BankingCardType;
 import dev.andrylat.bankingutilities.dialog.ProgressDialog;
+import dev.andrylat.bankingutilities.interfaces.CardTypeValidator;
 import dev.andrylat.bankingutilities.interfaces.Dialog;
 import dev.andrylat.bankingutilities.interfaces.CardValidator;
+import dev.andrylat.bankingutilities.interfaces.MortgagePayment;
+import dev.andrylat.bankingutilities.mortgagecalculator.MortgagePaymentImpl;
+import dev.andrylat.bankingutilities.validators.CardTypeValidatorImpl;
 import dev.andrylat.bankingutilities.validators.CardValidatorImpl;
 
 import java.util.List;
-import java.util.Map;
 
 public class MainApplication {
 
@@ -18,22 +22,40 @@ public class MainApplication {
 
         Dialog progressDialog = new ProgressDialog();
         CardValidator bankingCardValidator = new CardValidatorImpl();
-        progressDialog.setCustomerInteraction(bankingCardValidator);
-
+        progressDialog.setBankingCardValidator(bankingCardValidator);
         String customerCardNumber = progressDialog.getCustomerData();
 
-        List<String> errorResult = progressDialog.interactionWithCustomer(customerCardNumber);
-        if (errorResult.stream().findFirst().get().equalsIgnoreCase("ok"))
-            paymentSystem();
-        else {
+        List<String> errorResult = progressDialog.validateCustomerInput(customerCardNumber);
+        if (errorResult.stream().findFirst().get().equalsIgnoreCase("ok")) {
+            showPaymentSystem(customerCardNumber, progressDialog);
+            calculationMortgagePayments(progressDialog);
+            progressDialog.showMortgagePayment();
+        } else {
             progressDialog.showErrorsLog(errorResult);
             initMainApplication();
         }
     }
 
-    private static void paymentSystem() {
-        System.out.println("You are in main apps");
+    private static void showPaymentSystem(String customerCardNumber, Dialog progressDialog) {
+
+        BankingCardType[] bankingCardTypes = BankingCardType.values();
+        CardTypeValidator cardTypeValidator = new CardTypeValidatorImpl(Integer.parseInt(String.valueOf(customerCardNumber.toCharArray()[0])));
+        progressDialog.setCardTypeValidator(cardTypeValidator);
+        cardTypeValidator.setPossibleCards(bankingCardTypes);
+        progressDialog.showCardType();
+
     }
 
+    private static void calculationMortgagePayments(Dialog progressDialog) {
 
+        MortgagePayment mortgagePayment = new MortgagePaymentImpl();
+        progressDialog.setMortgagePayment(mortgagePayment);
+        mortgagePayment.setDetailsForCalculation(paymentSystem(progressDialog));
+        mortgagePayment.monthlyPaymentCalculation();
+
+    }
+
+    private static double[] paymentSystem(Dialog progressDialog) {
+        return progressDialog.customerPaymentMenu();
+    }
 }
