9390ab36a557c0d56b6de152cd12317fc7d60e9e
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-11-13 09:35:39 -0500

    Add new methods for interaction with customer in case of validation type

--- src/main/java/dev/andrylat/bankingutilities/dialog/ProgressDialog.java
+++ src/main/java/dev/andrylat/bankingutilities/dialog/ProgressDialog.java
@@ -1,7 +1,9 @@
 package dev.andrylat.bankingutilities.dialog;
 
+import dev.andrylat.bankingutilities.interfaces.CardTypeValidator;
 import dev.andrylat.bankingutilities.interfaces.Dialog;
 import dev.andrylat.bankingutilities.interfaces.CardValidator;
+import dev.andrylat.bankingutilities.interfaces.MortgagePayment;
 
 import java.util.List;
 import java.util.Scanner;
@@ -9,13 +11,26 @@ import java.util.Scanner;
 public class ProgressDialog implements Dialog {
 
     private CardValidator validatorInt;
+    private MortgagePayment mortgagePayment;
+    private CardTypeValidator cardTypeValidator;
 
+    @Override
+    public void setMortgagePayment(MortgagePayment mortgagePayment) {
+        this.mortgagePayment = mortgagePayment;
+    }
 
-    public void setCustomerInteraction(CardValidator validatorInt)  {
+    @Override
+    public void setBankingCardValidator(CardValidator validatorInt) {
         this.validatorInt = validatorInt;
     }
 
-    public  String getCustomerData() {
+    @Override
+    public void setCardTypeValidator(CardTypeValidator cardTypeValidator){
+        this.cardTypeValidator = cardTypeValidator;
+    }
+
+    @Override
+    public String getCustomerData() {
         System.out.println("Enter a card number with 16 digits");
         System.out.println("Ex : xxxx-xxxx-xxxx-xxxx or \n xxxx xxxx xxxx xxxx");
         Scanner scanner = new Scanner(System.in);
@@ -29,7 +44,30 @@ public class ProgressDialog implements Dialog {
         errorResult.forEach(System.out::println);
     }
 
-    public List<String> interactionWithCustomer(String customerInput) {
-        return validatorInt.customerDataTreatment(customerInput);
+    @Override
+    public List<String> validateCustomerInput(String customerInput) {
+        return validatorInt.validate(customerInput);
+    }
+
+    @Override
+    public void showCardType() {
+        System.out.println("Your payment system is  " + cardTypeValidator.customerCardType());
+    }
+
+    @Override
+    public double[] customerPaymentMenu() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Principal Amount : ");
+        double principal_Amount = scan.nextDouble();
+        System.out.print("Enter Rate of Interest : ");
+        double interestRate = scan.nextDouble();
+        System.out.print("Enter Time period in years : ");
+        double yearsPeriod = scan.nextInt();
+
+        return new double[]{principal_Amount, interestRate, yearsPeriod};
+    }
+    @Override
+    public void showMortgagePayment() {
+        System.out.println(mortgagePayment.toString());
     }
 }
